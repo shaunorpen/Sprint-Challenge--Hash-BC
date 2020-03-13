b25d876f139bd63c0d7b89feaf013fa4c2b3300b
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-13 11:01:20 +0000

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,15 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    i = 0
+    current_dest= "NONE"
+
+    while i < length - 1:
+        current_dest = hash_table_retrieve(hashtable, current_dest)
+        route[i] = current_dest
+        i += 1
+
+    return route[:-1]
